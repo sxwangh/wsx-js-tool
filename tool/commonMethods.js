1e6d240bc80db88b5cb4758875179ae78a96a515
sixuewang <sixuewang@emotibot.com>
2020-12-18 15:29:06 +0800

    location.href匹配

--- tool/commonMethods.js
+++ tool/commonMethods.js
@@ -1,7 +1,10 @@
 function getUrlParamByName(name) {
   let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+  let r = null;
   // location.search :设置/获取url？后面的部分 （?q=123&page=1&type=note）
-  let r = window.location.hash.split('#/?')[1].match(reg); // (q=123&page=1&type=note)
+  if (window.location.href.split('?')[1]) { // (q=123&page=1&type=note)
+    r = window.location.href.split('?')[1].match(reg); // /(^|&)enterpriseId=([^&]*)(&|$)/i
+  }
   if (r != null) {
     return decodeURIComponent(r[2]);
   };
